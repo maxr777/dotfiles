954aa00b48bbb0be937519c153fdf04ccb99ff58
max <>
2025-07-19 17:56:25 +0200

    check if there are andy dotfiles already and backup if there are

--- setup.sh
+++ setup.sh
@@ -11,4 +11,26 @@ mkdir -p ~/.tmux/plugins
 [ ! -d ~/.tmux/plugins/tmux-resurrect ] && git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect  
 [ ! -d ~/.tmux/plugins/tmux-continuum ] && git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
 
+backup_dir=~/dotfiles_backup_$(date +%Y%m%d_%H%M%S)
+
+backup_if_exists() {
+    local source="$1"
+    local name="$2"
+    
+    if [ -e "$source" ]; then
+        echo "Backing up existing $name..."
+        mkdir -p "$backup_dir"
+        mv "$source" "$backup_dir/"
+        return 0
+    fi
+    return 1
+}
+
+backup_if_exists ~/.bashrc "bashrc"
+backup_if_exists ~/.config/nvim "nvim config"
+backup_if_exists ~/.config/i3 "i3 config" 
+backup_if_exists ~/.tmux.conf "tmux config"
+
+[ -d "$backup_dir" ] && echo "Existing configs backed up to: $backup_dir"
+
 stow bashrc nvim i3 tmux
