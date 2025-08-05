88b7b50fdc2b370aa0dff03da5d2ce08a17f2535
max <maxr777@proton.me>
2025-08-05 22:10:10 +0200

    Add mpv and redshift configs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dnf install -y i3 neovim tmux stow alacritty rofi flameshot fd-find bat xinput brightnessctl dunst thunar nmtui
+sudo dnf install -y i3 neovim tmux stow alacritty rofi flameshot fd-find bat xinput brightnessctl dunst thunar nmtui redshift
 
 xdg-settings set default-file-manager thunar.desktop
 
@@ -38,4 +38,4 @@ backup_if_exists ~/.config/alacritty "alacritty config"
 
 [ -d "$backup_dir" ] && echo "Existing configs backed up to: $backup_dir"
 
-stow bashrc nvim i3 tmux alacritty i3status dunst claude
+stow bashrc nvim i3 tmux alacritty i3status dunst claude mpv redshift
