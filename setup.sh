f6b86a522a66b572aa7bb8ac01a09d9174b11e84
max <maxr777@proton.me>
2026-02-21 09:09:42 +0100

    xset

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dnf install -y i3 neovim tmux stow alacritty rofi flameshot fd-find bat xinput brightnessctl dunst thunar nmtui redshift zathura-pdf-mupdf unclutter libnotify python3 ibus ibus-libpinyin
+sudo dnf install -y i3 neovim tmux stow alacritty rofi flameshot fd-find bat xinput brightnessctl dunst thunar nmtui redshift zathura-pdf-mupdf unclutter libnotify python3 ibus ibus-libpinyin xset
 
 xdg-settings set default-file-manager thunar.desktop
 
